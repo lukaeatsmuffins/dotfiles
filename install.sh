d42ebb2513c81573e5cc3111dfe1d3cb1216fa1e
Lambda <118488231+lukaeatsmuffins@users.noreply.github.com>
2024-10-26 22:46:05 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pacman -S --noconfirm hyprlock waybar wofi dolphin swww
+#pacman -S --noconfirm git vim kitty pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse swaync firefox
+# pacman -S --noconfirm linux-headers nvidia-dkms nvidia-utils egl-wayland hyprland hyprpaper hyprpicker hypridle hyprcursor qt5-wayland qt6-wayland
